d25e34f367d67991ddc64a914d4b267b5c9efb11
ushagour <55017548+ushagour@users.noreply.github.com>
2020-05-17 02:30:43 +0000

    rrfy

--- application/views/globals/header.php
+++ application/views/globals/header.php
@@ -6,15 +6,15 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-<!-- <link href="<?=base_url()?>assets/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+	<!-- <link href="<?=base_url()?>assets/css/font-awesome.min.css" rel="stylesheet">
   <script type="text/javascript" src="<?=base_url()?>assets/js/fontawesome.min.js"></script> 
 -->
 	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
 
-	 <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
-  <script type="text/javascript" src="<?=base_url()?>assets/js/bootstrap.min.js"></script> 
+	<link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+	<script type="text/javascript" src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
 
 
 
@@ -39,28 +39,31 @@
 					<h4>gestion RH : <small>mr </small> <button type="button" class="btn btn-default btn-sm">
 							<span class="glyphicon glyphicon-log-out"></span> Log out
 						</button></h4>
-						<ul class="navbar-mobile__list list-unstyled">
-					
-                        <li class="has-sub <?php if($this->session->userdata('menu') =="home")echo "active"; else  echo " ";?>">
-						<a href="<?=base_url()?>home">
+					<ul class="navbar-mobile__list list-unstyled">
+
+						<li
+							class="has-sub <?php if($this->session->userdata('menu') =="home")echo "active"; else  echo " ";?>">
+							<a href="<?=base_url()?>home">
 								<i class="fas fa-home mr-3"></i> Home</a>
-                       
+
 						</li>
 
-                        <li class="has-sub <?php if($this->session->userdata('menu') =="ajouter") echo "active"; else " ";?>">
-						<a  href="<?=base_url()?>ajouter">
-            <i class="fas fa-user-plus mr-3"></i> 	Ajouter </a>
-                        </li>
-					   
-						
-                        <li class="has-sub <?php if($this->session->userdata('menu') == "edit")echo "active"; else " ";?>">
-						<a href="<?=base_url()?>edit">
-            <i class="fas fa-edit mr-3"></i>   editier </a>
+						<li
+							class="has-sub <?php if($this->session->userdata('menu') =="ajouter") echo "active"; else " ";?>">
+							<a href="<?=base_url()?>ajouter">
+								<i class="fas fa-user-plus mr-3"></i> Ajouter </a>
 						</li>
-						
 
-                   </ul>
-<br>
+
+						<li
+							class="has-sub <?php if($this->session->userdata('menu') == "edit")echo "active"; else " ";?>">
+							<a href="<?=base_url()?>edit">
+								<i class="fas fa-edit mr-3"></i> editier </a>
+						</li>
+
+
+					</ul>
+					<br>
 
 
 
