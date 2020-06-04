af5a893f2a7604800595e1c80c4b5de716b2ed81
ushagour <55017548+ushagour@users.noreply.github.com>
2020-06-04 23:29:53 +0100

    scroll up item

--- application/views/perssonel/see_page.php
+++ application/views/perssonel/see_page.php
@@ -150,7 +150,7 @@
 
 		</div>
 
-
+		<a href="#" title="Haut de page" class=" scrollup " ><i class="fa fa-arrow-up"></i></a>
 
 		</div>
 
@@ -199,4 +199,31 @@
 
 	}
 
+
+
+	ScrollToTop=function() {
+  var s = $(window).scrollTop();
+  if (s > 250) {
+    $('.scrollup').fadeIn();
+  } else {
+    $('.scrollup').fadeOut();
+  }
+
+  $('.scrollup').click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 500);
+      return false;
+  });
+}
+
+StopAnimation=function() {
+  $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(){
+    $('html, body').stop();
+  });
+}
+
+
+$(window).scroll(function() {
+  ScrollToTop();
+  StopAnimation();
+});
 </script>
