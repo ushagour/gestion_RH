16a3d00849a5bdf34d85309507c5ddef981cf4d8
ushagour <55017548+ushagour@users.noreply.github.com>
2020-06-26 14:48:31 +0100

    encryption key initalisation in config

--- application/config/config.php
+++ application/config/config.php
@@ -325,7 +325,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'KaPdSgV';
+$config['encryption_key'] = ENC_KEY;
 
 /*
 |--------------------------------------------------------------------------
